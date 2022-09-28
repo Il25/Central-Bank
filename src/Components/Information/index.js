010734a570067bb28250d2caa46cca7e7e478020
Ilona Ch <chernyavskaya.ilona25@gmail.com>
2022-09-28 16:04:30 +0400

    slider

---
+++ src/Components/Information/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import "./index.css";
+
+const Information = () => {
+
+
+    return (
+        <div className="information_wrapper">
+            jj
+        </div>       
+    );
+};
+
+export default Information;
